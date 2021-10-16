011b6a0f4aaf08330c5849e13cf0597fb62d743c
DrMagicalStone <45792652+DrMagicalStone@users.noreply.github.com>
2021-10-16 13:25:24 +0800

    Removed CompleteInitEvent

--- pages/game/launch.ts
+++ pages/game/launch.ts
@@ -1,8 +1,6 @@
 import { initializeEventActions, processBegin } from "pages/game/game_process";
 import the_stage = require("pages/game/the_stage");
 import game_process = require("pages/game/game_process");
-import { event_bus } from "js/event/eventbus";
-import { CompleteInitEvent } from "js/event/CompleteInitEvent";
 
 
 function subscribeEvents(): void
@@ -15,6 +13,9 @@ function subscribeEvents(): void
  * Entry of the game
  */
 
+/**
+ * 
+ */
 initializeEventActions();
 
 console.log("event action initialized.");
@@ -23,6 +24,6 @@ subscribeEvents();
 
 console.log("event subscriber initialized.");
 
-event_bus.post(new CompleteInitEvent(), undefined, processBegin);
+processBegin();
 
 console.log("process began.");
