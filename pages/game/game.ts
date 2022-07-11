52843eda7c6eff0adb2493d7108dd587bed474f9
Ozelot Vanilla <shuihenyun@qq.com>
2022-07-11 19:49:15 +0800

    Try to fix pause feature
    Causing browser refreshing canvas.

--- pages/game/game.ts
+++ pages/game/game.ts
@@ -6,19 +6,43 @@ import { game_clock } from "pages/game/game_clock";
 export function pauseGame(callback: () => void = undefined): void
 {
     game_clock.setTimeRate(0);
-    $("body")[0].innerHTML = $("body")[0].innerHTML +
-        `
-            <div id="pause_background"
-                style="background-color: #00000030; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 100;" >
-                <div id="pause_tip"
-                    style="background-color: #ffffff; position: absolute; left: 50%; top: 50%;
-                           -webkit-transform: translate(-50%,-50%);
-                           -moz-transform: translate(-50%,-50%);
-                           transform:translate(-50%,-50%);">
-                    Game paused, press "space to start again"
-                </div>
-            </div>
-        `;
+
+    let pause_background: HTMLDivElement = document.createElement("div");
+    let pause_tip: HTMLDivElement = document.createElement("div");
+
+    pause_background.id = "pause_background";
+    pause_background.setAttribute(
+        "style",
+        "background-color: #00000030; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 100;"
+    );
+
+    pause_tip.id = "pause_tip";
+    pause_tip.setAttribute(
+        "style",
+        `background-color: #ffffff; 
+        position: absolute; left: 50%; top: 50%;
+        -webkit-transform: translate(-50%,-50%);
+        -moz-transform: translate(-50%,-50%);
+        transform:translate(-50%,-50%);`
+    );
+    pause_tip.innerText = "Game paused, press \"space\" to start again";
+
+    pause_background.appendChild(pause_tip);
+    document.body.appendChild(pause_background);
+
+    // $("body")[0].innerHTML = $("body")[0].innerHTML +
+    //     `
+    //         <div id="pause_background"
+    //             style="background-color: #00000030; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 100;" >
+    //             <div id="pause_tip"
+    //                 style="background-color: #ffffff; position: absolute; left: 50%; top: 50%;
+    //                        -webkit-transform: translate(-50%,-50%);
+    //                        -moz-transform: translate(-50%,-50%);
+    //                        transform:translate(-50%,-50%);">
+    //                 Game paused, press "space" to start again
+    //             </div>
+    //         </div>
+    //     `;
     $("#pause_background, #pause_tip").trigger("click", resumeGame);
     if (callback !== undefined)
     {
